95ef0a86302ee65a870e6c847914ca896af984ff
nathaniel-alvin <alvinparali@gmail.com>
2024-10-22 15:07:08 +0700

    feature: create ship with asteroids

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # groups
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
+
+    Player.containers = (updatable_group, drawable_group)
+    _ = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroid_group, updatable_group, drawable_group)
+    AsteroidField.containers = updatable_group
+    _ = AsteroidField()
+
+    # game loop
     while True:
+        # limit to 60 FPS
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
+
+        screen.fill("black")
+
+        for updatable in updatable_group:
+            updatable.update(dt)
+        for drawable in drawable_group:
+            drawable.draw(screen)
+
+        # refresh/update screen
         pygame.display.flip()
 
 
-main()
+if __name__ == "__main__":
+    main()
